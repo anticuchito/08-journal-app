b545b14f2efb56e09df08dc9174c6d7c06875ffb
anticuchito <macroo14@gmail.com>
2020-11-12 03:03:15 -0300

    add enviorment vars

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -1,37 +1,34 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
-import dotenv from 'dotenv'
-dotenv.config();
-console.log(process.env)
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCiErJ6dl2eXkrwOwlnYsNB84BWUD7BMs0",
-    authDomain: "react-app-curso-bf3c4.firebaseapp.com",
-    databaseURL: "https://react-app-curso-bf3c4.firebaseio.com",
-    projectId: "react-app-curso-bf3c4",
-    storageBucket: "react-app-curso-bf3c4.appspot.com",
-    messagingSenderId: "431121096391",
-    appId: "1:431121096391:web:04a7ebf300e0a1d21da8e2"
+    apiKey:process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DB_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket:  process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId:  process.env.REACT_APP_MSG_SENDER_ID,
+    appId:  process.env.REACT_APP_APP_ID
   };
 
-  const firebaseConfigTesting = {
-    apiKey: "AIzaSyD0mpJd-hH9nQlk-w_GRcRRSAOrFkZVEPY",
-    authDomain: "sql-demos-69035.firebaseapp.com",
-    databaseURL: "https://sql-demos-69035.firebaseio.com",
-    projectId: "sql-demos-69035",
-    storageBucket: "sql-demos-69035.appspot.com",
-    messagingSenderId: "687609693162",
-    appId: "1:687609693162:web:060cb1a604d4a092492893"
-  };
+//   const firebaseConfigTesting = {
+//     apiKey: "AIzaSyD0mpJd-hH9nQlk-w_GRcRRSAOrFkZVEPY",
+//     authDomain: "sql-demos-69035.firebaseapp.com",
+//     databaseURL: "https://sql-demos-69035.firebaseio.com",
+//     projectId: "sql-demos-69035",
+//     storageBucket: "sql-demos-69035.appspot.com",
+//     messagingSenderId: "687609693162",
+//     appId: "1:687609693162:web:060cb1a604d4a092492893"
+//   };
 
- // Initialize Firebase
-if(process.env.NODE_ENV === 'test') {
-  firebase.initializeApp(firebaseConfigTesting);
-}else{
-  firebase.initializeApp(firebaseConfig);
-}
-
+//  // Initialize Firebase
+// if(process.env.NODE_ENV === 'test') {
+//   firebase.initializeApp(firebaseConfigTesting);
+// }else{
+//   firebase.initializeApp(firebaseConfig);
+// }
+firebase.initializeApp(firebaseConfig);
 
  
 
